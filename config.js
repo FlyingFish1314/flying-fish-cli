0a0b1390d1a765dad537e5220fb5b244dae65bd1
范恩龙 <1732819972@qq.com>
2023-04-17 08:44:17 +0800

    更新集成element-plus的vue3-vite-template_element_plus

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ const framworkUrl = {
   express: 'https://gitee.com/Flying--fish/express-template.git',
   koa: 'https://gitee.com/Flying--fish/koa-template.git',
   egg: 'https://gitee.com/Flying--fish/egg-template.git',
-  vue3_vite_ts: 'https://gitee.com/Flying--fish/vue3-vite-template.git'
+  vue3_vite_ts: 'https://gitee.com/Flying--fish/vue3-vite-template.git',
+  vue3_vite_ts_element_plus: 'https://gitee.com/Flying--fish/vue3-vite-template.git#element-plus'
 }
 export {
   framwork,
