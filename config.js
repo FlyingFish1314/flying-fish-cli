6cfed13b0718894c0c35385e15caa5d89b241361
范恩龙 <1732819972@qq.com>
2023-04-27 15:18:45 +0800

    add nust3-template

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
 // 可选择的框架
-const framwork = ['express', 'koa', 'egg', 'vue3_vite_ts', 'vue3_vite_ts_element_plus']
+const framwork = ['express', 'koa', 'egg', 'vue3_vite_ts', 'vue3_vite_ts_element_plus',
+  'nust3_template']
 // 框架对应的下载地址
 const framworkUrl = {
   express: 'https://gitee.com/Flying--fish/express-template.git',
   koa: 'https://gitee.com/Flying--fish/koa-template.git',
   egg: 'https://gitee.com/Flying--fish/egg-template.git',
   vue3_vite_ts: 'https://gitee.com/Flying--fish/vue3-vite-template.git',
-  vue3_vite_ts_element_plus: 'https://gitee.com/Flying--fish/vue3-vite-template.git#element-plus'
+  vue3_vite_ts_element_plus: 'https://gitee.com/Flying--fish/vue3-vite-template.git#element-plus',
+  nust3_template: 'https://gitee.com/Flying--fish/nust3-template.git'
 }
 export {
   framwork,
